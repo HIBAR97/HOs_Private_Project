562c730a29c85cf9738cb5ec95576ee1446dae16
HIBER97 <hibar9710@gmail.com>
2022-07-04 14:04:13 +0900

    2022. 7. 4. PM 2:03 --------------------------------------- ADD - Login_Activity, Signin_Activity - 로그인, 회원가입 페이지 추가 --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix - - ---------------------------------------

---
+++ app/src/main/java/com/example/Login_Activity.java
@@ -0,0 +1,6 @@
+package com.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login_Activity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/Signin_Activity.java
@@ -0,0 +1,6 @@
+package com.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Signin_Activity extends AppCompatActivity {
+}
