dfdfa9c8027ae08d5b0fc24941e764103687c972
HIBER97 <hibar9710@gmail.com>
2022-08-08 17:13:22 +0900

    2022. 8. 8. PM 5:12 --------------------------------------- ADD - MyRecipe2_Fragment 추가 - 나의 레시피2 화면 추가 - 연동 완료 --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix - - ---------------------------------------

--- app/src/main/java/com/example/MyRecipe_Fragment.java
+++ app/src/main/java/com/example/MyRecipe_Fragment.java
@@ -4,20 +4,47 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MyRecipe_Fragment extends Fragment {
 
     private View view;
+
+    private FragmentManager fragmentManager;
+    private FragmentTransaction transaction;
+
+    private MyRecipe2_Fragment myRecipe2_fragment;
+
+
+
     @Nullable
     @Override
     public View onCreateView (@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_myrecipe, container, false);
 
+        fragmentManager = getActivity().getSupportFragmentManager();
+
+        myRecipe2_fragment = new MyRecipe2_Fragment();
+
+        //-------------- 선언 ---------------//
+        Button BtnMake = view.findViewById(R.id.Frg_myrecipe_BtnMake);
+        Button BtnList = view.findViewById(R.id.Frg_myrecipe_BtnList);
+
+        //----------- 리스너 -----------------//
+        BtnMake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.MF_FrameLayout, myRecipe2_fragment).commitAllowingStateLoss();
+            }
+        });
         return view;
     }
 
