0236f621fa05fdf33b6b8f1359f8ea36a93c213d
HIBER97 <hibar9710@gmail.com>
2022-08-17 23:35:38 +0900

    2022. 8. 17. PM 11:32 --------------------------------------- ADD - MyRecipe6_3_Fragment 추가 - 나의 레시피 6_3페이지 추가 - 버튼 연결 완료 1 <-> 6_3 까지 전환 가능 --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix - - ---------------------------------------

--- app/src/main/java/com/example/MyRecipe6_3_Fragment.java
+++ app/src/main/java/com/example/MyRecipe6_3_Fragment.java
@@ -12,48 +12,47 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-public class MyRecipe6_2_Fragment extends Fragment {
+public class MyRecipe6_3_Fragment extends Fragment {
     private View view;
 
     private FragmentManager fragmentManager;
     private FragmentTransaction transaction;
 
-    private MyRecipe6_1_Fragment myRecipe6_1_fragment;
-    private MyRecipe6_3_Fragment myRecipe6_3_fragment;
+    private MyRecipe6_2_Fragment myRecipe6_2_fragment;
+//    private MyRecipe6_3_Fragment myRecipe6_2_fragment;
 
     @Nullable
     @Override
     public View onCreateView (@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
 
-        view = inflater.inflate(R.layout.fragment_myrecipe6_2, container, false);
+        view = inflater.inflate(R.layout.fragment_myrecipe6_3, container, false);
 
         fragmentManager = getActivity().getSupportFragmentManager();
 
-        myRecipe6_1_fragment = new MyRecipe6_1_Fragment();
-        myRecipe6_3_fragment = new MyRecipe6_3_Fragment();
+        myRecipe6_2_fragment = new MyRecipe6_2_Fragment();
+//        myRecipe6_3_fragment = new MyRecipe6_2_Fragment();
 
         //----------선언--------------//
-        Button btn_Prev = view.findViewById(R.id.Frg_myrecipe6_2_Prev);
-        Button btn_Next = view.findViewById(R.id.Frg_myrecipe6_2_Next);
+        Button btn_Prev = view.findViewById(R.id.Frg_myrecipe6_3_Prev);
+        Button btn_Next = view.findViewById(R.id.Frg_myrecipe6_3_Next);
 
         //---------리스너----------//
         btn_Prev.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 transaction = fragmentManager.beginTransaction();
-                transaction.replace(R.id.MF_FrameLayout, myRecipe6_1_fragment).commitAllowingStateLoss();
+                transaction.replace(R.id.MF_FrameLayout, myRecipe6_2_fragment).commitAllowingStateLoss();
             }
         });
 
         btn_Next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                transaction = fragmentManager.beginTransaction();
-                transaction.replace(R.id.MF_FrameLayout, myRecipe6_3_fragment).commitAllowingStateLoss();
+//                transaction = fragmentManager.beginTransaction();
+//                transaction.replace(R.id.MF_FrameLayout, myRecipe6_2_fragment).commitAllowingStateLoss();
             }
         });
         return view;
     }
 }
-
