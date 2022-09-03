a1c57edf2e0f08212cc9b5f3cd5d3681820d3c56
HIBER97 <hibar9710@gmail.com>
2022-09-03 20:35:39 +0900

    2022. 9. 3. PM 8:35 --------------------------------------- ADD - 리스트 목록 추가중 - --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix - - ---------------------------------------

--- app/src/main/java/com/example/MyRecipe6_1_Fragment.java
+++ app/src/main/java/com/example/MyRecipe6_1_Fragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,6 +23,8 @@ public class MyRecipe6_1_Fragment extends Fragment {
     private MyRecipe5_Fragment myRecipe5_fragment;
     private MyRecipe6_2_Fragment myRecipe6_2_fragment;
 
+    String[] Temp = {"90","91","92","93","94","95","96"};
+
     @Nullable
     @Override
     public View onCreateView (@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -33,9 +37,11 @@ public class MyRecipe6_1_Fragment extends Fragment {
         myRecipe5_fragment = new MyRecipe5_Fragment();
         myRecipe6_2_fragment = new MyRecipe6_2_Fragment();
 
+
         //----------선언--------------//
         Button btn_Prev = view.findViewById(R.id.Frg_myrecipe6_1_Prev);
         Button btn_Next = view.findViewById(R.id.Frg_myrecipe6_1_Next);
+        Spinner spinner_Temp = view.findViewById(R.id.Frg_myrecipe6_Spinner1);
 
         //---------리스너----------//
         btn_Prev.setOnClickListener(new View.OnClickListener() {
@@ -53,6 +59,12 @@ public class MyRecipe6_1_Fragment extends Fragment {
                 transaction.replace(R.id.MF_FrameLayout, myRecipe6_2_fragment).addToBackStack(null).commit();
             }
         });
+
+        //---------어뎁터----------//
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.fragment_myrecipe6_1,spinner_Temp);
+//        spinner_Temp.setAdapter(adapter);
+//        spinner_Temp.setSelection(0);
+
         return view;
     }
 }
