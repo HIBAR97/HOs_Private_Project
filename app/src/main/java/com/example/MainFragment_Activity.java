78ce881e3bd9e53d4a8e6204ed54090ecb93b453
HIBER97 <hibar9710@gmail.com>
2022-09-05 17:02:26 +0900

    2022. 9. 5. PM 5:01 --------------------------------------- ADD - - --------------------------------------- Delete - - --------------------------------------- Error - 사이드 메뉴 함수 추가 작동X - 저번 오류 아직 픽스 X --------------------------------------- Fix - - ---------------------------------------

--- app/src/main/java/com/example/MainFragment_Activity.java
+++ app/src/main/java/com/example/MainFragment_Activity.java
@@ -4,12 +4,19 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -110,6 +117,74 @@ public class MainFragment_Activity extends AppCompatActivity {
             }
         });
 
+    }
+    /***
+     *  -> 사이드 네브바 세팅
+     *   - 클릭 아이콘 설정
+     *   - 아이템 클릭 이벤트 설정
+     */
+    public void settingSideNavBar()
+    {
+        // 툴바 생성
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        // 사이드 메뉴를 오픈하기위한 아이콘 추가
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_dehaze_48);
+
+        // 사이드 네브바 구현
+//        DrawerLayout drawLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                MainFragment_Activity.this,
+                drawLayout,
+                toolbar,
+                R.string.open,
+                R.string.closed
+        );
+
+        // 사이드 네브바 클릭 리스너
+        drawLayout.addDrawerListener(actionBarDrawerToggle);
+
+        // -> 사이드 네브바 아이템 클릭 이벤트 설정
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                int id = menuItem.getItemId();
+
+                if (id == R.id.menu_item1){
+                    Toast.makeText(getApplicationContext(), "메뉴아이템 1 선택", Toast.LENGTH_SHORT).show();
+                }else if(id == R.id.menu_item2){
+                    Toast.makeText(getApplicationContext(), "메뉴아이템 2 선택", Toast.LENGTH_SHORT).show();
+                }else if(id == R.id.menu_item3){
+                    Toast.makeText(getApplicationContext(), "메뉴아이템 3 선택", Toast.LENGTH_SHORT).show();
+                }
+
+
+                DrawerLayout drawer = findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
 
     }
+
+
+    /***
+     *  -> 뒤로가기시, 사이드 네브바 닫는 기능
+     */
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
+
