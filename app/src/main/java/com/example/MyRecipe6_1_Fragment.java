321b93734553178b37735c31441e983097e2c3f3
HIBER97 <hibar9710@gmail.com>
2022-09-22 22:38:06 +0900

    2022. 9. 22. PM 10:37 --------------------------------------- ADD - 추출 6_1의 모든 스피너에 더미값이 추가 되었습니다 - --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix - - ---------------------------------------

--- app/src/main/java/com/example/MyRecipe6_1_Fragment.java
+++ app/src/main/java/com/example/MyRecipe6_1_Fragment.java
@@ -23,8 +23,6 @@ public class MyRecipe6_1_Fragment extends Fragment {
     private MyRecipe5_Fragment myRecipe5_fragment;
     private MyRecipe6_2_Fragment myRecipe6_2_fragment;
 
-    //String[] Temp = {"90","91","92","93","94","95","96"};
-
     @Nullable
     @Override
     public View onCreateView (@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -42,6 +40,8 @@ public class MyRecipe6_1_Fragment extends Fragment {
         Button btn_Prev = view.findViewById(R.id.Frg_myrecipe6_1_Prev);
         Button btn_Next = view.findViewById(R.id.Frg_myrecipe6_1_Next);
         Spinner spinner_Temp = view.findViewById(R.id.Frg_myrecipe6_Spinner1);
+        Spinner spinner_Water = view.findViewById(R.id.Frg_myrecipe6_Spinner2);
+        Spinner spinner_Water_Flow = view.findViewById(R.id.Frg_myrecipe6_Spinner3);
 
         //---------리스너----------//
         btn_Prev.setOnClickListener(new View.OnClickListener() {
@@ -60,11 +60,18 @@ public class MyRecipe6_1_Fragment extends Fragment {
             }
         });
 
-        //---------어뎁터----------//
+        //---------스피너 어뎁터----------//
         ArrayAdapter<CharSequence> Temp_adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.Temp, android.R.layout.simple_spinner_dropdown_item);
         spinner_Temp.setAdapter(Temp_adapter);
-        //spinner_Temp.adapter = Temp_adapter;
         spinner_Temp.setSelection(0);
+
+        ArrayAdapter<CharSequence> Water_adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.Water_mess, android.R.layout.simple_spinner_dropdown_item);
+        spinner_Water.setAdapter(Water_adapter);
+        spinner_Water.setSelection(0);
+
+        ArrayAdapter<CharSequence> Water_Flow_adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.Water_Flow, android.R.layout.simple_spinner_dropdown_item);
+        spinner_Water_Flow.setAdapter(Water_Flow_adapter);
+        spinner_Water_Flow.setSelection(0);
 
         return view;
     }
