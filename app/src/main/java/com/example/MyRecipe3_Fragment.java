0f14063b64021513d7bc33ead761a025ebdcb71a
HIBER97 <hibar9710@gmail.com>
2022-09-25 15:18:20 +0900

    2022. 9. 25. PM 3:17 --------------------------------------- ADD - Array_data 추가 - 레시피 3까지 연결 --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix - - ---------------------------------------

--- app/src/main/java/com/example/MyRecipe3_Fragment.java
+++ app/src/main/java/com/example/MyRecipe3_Fragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -38,6 +40,11 @@ public class MyRecipe3_Fragment extends Fragment {
         Button btn_Prev = view.findViewById(R.id.Frg_myrecipe3_Prev);
         Button btn_Next = view.findViewById(R.id.Frg_myrecipe3_Next);
 
+        Spinner spinner_Recommend_Water = view.findViewById(R.id.Frg_myrecipe3_Spinner1);
+        Spinner spinner_Crushing_Degree = view.findViewById(R.id.Frg_myrecipe3_Spinner2);
+        Spinner spinner_Roasting = view.findViewById(R.id.Frg_myrecipe3_Spinner3);
+        Spinner spinner_Production_Year = view.findViewById(R.id.Frg_myrecipe3_Spinner4);
+
         //---------리스너----------//
         btn_Prev.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,6 +62,22 @@ public class MyRecipe3_Fragment extends Fragment {
             }
         });
 
+        ArrayAdapter<CharSequence> Coffee_Recommend_Water_adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.Recommend_Water, android.R.layout.simple_spinner_dropdown_item);
+        spinner_Recommend_Water.setAdapter(Coffee_Recommend_Water_adapter);
+        spinner_Recommend_Water.setSelection(0);
+
+        ArrayAdapter<CharSequence> Coffee_Crushing_Degree_adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.Crushing_Degree, android.R.layout.simple_spinner_dropdown_item);
+        spinner_Crushing_Degree.setAdapter(Coffee_Crushing_Degree_adapter);
+        spinner_Crushing_Degree.setSelection(0);
+
+        ArrayAdapter<CharSequence> Coffee_Roasting_adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.Roasting, android.R.layout.simple_spinner_dropdown_item);
+        spinner_Roasting.setAdapter(Coffee_Roasting_adapter);
+        spinner_Roasting.setSelection(0);
+
+
+        ArrayAdapter<CharSequence> Coffee_Production_Year_adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.Production_Year, android.R.layout.simple_spinner_dropdown_item);
+        spinner_Production_Year.setAdapter(Coffee_Production_Year_adapter);
+        spinner_Production_Year.setSelection(0);
         return view;
     }
 }
