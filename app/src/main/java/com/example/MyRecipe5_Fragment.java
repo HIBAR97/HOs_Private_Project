fd398317b7492b6280f80b0476bb5cd15a3aeb8c
HIBER97 <hibar9710@gmail.com>
2022-09-28 13:18:20 +0900

    2022. 9. 28. PM 1:17 --------------------------------------- ADD - 나의 레시피 5 더미데이터 추가 - Array_data.xml 에 더미데이터 추가 --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix - UI 마이너 개선 - ---------------------------------------

--- app/src/main/java/com/example/MyRecipe5_Fragment.java
+++ app/src/main/java/com/example/MyRecipe5_Fragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -37,6 +39,11 @@ public class MyRecipe5_Fragment extends Fragment {
         Button btn_Prev = view.findViewById(R.id.Frg_myrecipe5_Prev);
         Button btn_Next = view.findViewById(R.id.Frg_myrecipe5_Next);
 
+        Spinner spinner_Temp = view.findViewById(R.id.Frg_myrecipe5_spinner1);
+        Spinner spinner_Input_Water = view.findViewById(R.id.Frg_myrecipe5_spinner2);
+        Spinner spinner_Water_Flow = view.findViewById(R.id.Frg_myrecipe5_spinner3);
+        Spinner spinner_Extraction_Time = view.findViewById(R.id.Frg_myrecipe5_spinner4);
+
         //---------리스너----------//
         btn_Prev.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +60,22 @@ public class MyRecipe5_Fragment extends Fragment {
                 transaction.replace(R.id.MF_FrameLayout, myRecipe6_1_fragment).addToBackStack(null).commit();
             }
         });
+
+        ArrayAdapter<CharSequence> spinner_Temp_adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.Temp, android.R.layout.simple_spinner_dropdown_item);
+        spinner_Temp.setAdapter(spinner_Temp_adapter);
+        spinner_Temp.setSelection(0);
+
+        ArrayAdapter<CharSequence> spinner_Input_Water_adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.Input_Water, android.R.layout.simple_spinner_dropdown_item);
+        spinner_Input_Water.setAdapter(spinner_Input_Water_adapter);
+        spinner_Input_Water.setSelection(0);
+
+        ArrayAdapter<CharSequence> spinner_Water_Flow_adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.Water_Flow, android.R.layout.simple_spinner_dropdown_item);
+        spinner_Water_Flow.setAdapter(spinner_Water_Flow_adapter);
+        spinner_Water_Flow.setSelection(0);
+
+        ArrayAdapter<CharSequence> spinner_Extraction_Time_adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.Extration_Time, android.R.layout.simple_spinner_dropdown_item);
+        spinner_Extraction_Time.setAdapter(spinner_Extraction_Time_adapter);
+        spinner_Extraction_Time.setSelection(0);
         return view;
     }
 }
